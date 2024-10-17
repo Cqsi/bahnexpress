812434106266a0f845c46157b1d86c198592cabe
Casimir Rönnlöf <casimirr04@gmail.com>
2024-10-17 08:56:54 +0300

    renewed mobile responsiveness, centered text

--- components/hero2.tsx
+++ components/hero2.tsx
@@ -1,13 +1,13 @@
 export default function Hero2() {
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-center lg:items-center min-h-[80vh] max-w-8xl mx-auto">
+    <div className="flex flex-col lg:flex-row items-center justify-center min-h-[80vh] max-w-8xl mx-auto">
       {/* Text Column */}
       <div className="lg:w-1/2 p-6 lg:pl-16 lg:pr-8">
-          <div className="flex flex-col items-start">
-            <h1 className="text-5xl md:text-6xl font-customFont leading-tight mb-4">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1 className="text-5xl md:text-6xl font-customFont leading-tight mb-4 text-center lg:text-left">
               Nopeat ja luotettavat autonsiirrot
             </h1>
-            <p className="text-xl text-gray-600 mb-6">
+            <p className="text-xl text-gray-600 mb-6 text-center lg:text-left">
               Tarjoamme ammattitaitoista ja luotettavaa autonsiirtopalvelua kaikkialla Suomessa.
             </p>
             <button className=" text-black text-xl border-2 border-black rounded-lg px-6 py-2 hover:bg-[#C4E0F9] transition-colors duration-300">
