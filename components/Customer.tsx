199c65ec5c61e9893a520203c9e14b400d974775
Casimir Rönnlöf <casimirr04@gmail.com>
2024-10-28 22:13:35 +0200

    .

--- components/Customer.tsx
+++ components/Customer.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import React, { useState, useEffect, useRef } from 'react';
 import CountUp from 'react-countup';
@@ -30,32 +30,34 @@ export default function Customer() {
   }, []);
 
   return (
-    <div className="flex flex-col md:flex-row mx-auto my-8">
+    <div className="flex flex-col md:flex-row mx-auto my-8 px-4 md:px-10">
       {/* Left Column */}
-      <div className="flex-1 flex items-center justify-center mx-10 p-8">
-        <p className="text-left text-5xl font-bold text-black">
+      <div className="flex-1 flex items-center justify-center p-4 md:p-8">
+        <p className="text-left text-4xl md:text-5xl font-bold text-black">
           Kokonaisvaltainen ja ketterä kuljetusyhtiö, joka on luottamuksen arvoinen.
         </p>
       </div>
       {/* Right Column */}
       <div className="flex-1 flex flex-col justify-center gap-4">
         {/* Top Row */}
-        <div className="flex-1 flex items-center justify-center p-8">
+        <div className="flex-1 flex items-center justify-center p-4 md:p-8">
           <div className="flex flex-col items-center">
-            <p className="text-4xl font-semibold text-center text-black z-10">Asiakastyytyväisyys</p>
+            <p className="text-3xl md:text-4xl font-semibold text-center text-black z-10">
+              Asiakastyytyväisyys
+            </p>
             <div className="flex items-center">
-              <p className="text-4xl font-semibold text-black mr-2">5,0</p>
-              <img src="/stars.jpg" alt="Stars rating" className="w-64 h-auto" />
+              <p className="text-3xl md:text-4xl font-semibold text-black mr-2">5,0</p>
+              <img src="/stars.jpg" alt="Stars rating" className="w-32 md:w-64 h-auto" />
             </div>
           </div>
         </div>
         {/* Bottom Row */}
-        <div className="flex-1 flex items-center justify-center p-8">
+        <div className="flex-1 flex items-center justify-center p-4 md:p-8">
           <div className="flex flex-col items-center">
-            <p className="text-4xl font-semibold text-center text-black">
+            <p className="text-3xl md:text-4xl font-semibold text-center text-black">
               Ulkomailta ajettua ajoneuvoja asiakkaille
             </p>
-            <p className="text-6xl font-semibold text-center text-black mt-3" ref={countUpRef}>
+            <p className="text-5xl md:text-6xl font-semibold text-center text-black mt-3" ref={countUpRef}>
               +
               {isVisible && (
                 <CountUp
